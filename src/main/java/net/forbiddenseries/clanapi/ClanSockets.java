aef6ac2a90def8439669fbabb355631a49ab4004
Nitidez <megazoor123@hotmail.com>
2025-04-15 15:01:25 +0000

    getClans

--- src/main/java/net/forbiddenseries/clanapi/ClanSockets.java
+++ src/main/java/net/forbiddenseries/clanapi/ClanSockets.java
@@ -7,18 +7,25 @@ import com.google.gson.JsonObject;
 
 import net.sacredlabyrinth.phaed.simpleclans.Clan;
 import net.sacredlabyrinth.phaed.simpleclans.managers.ClanManager;
+import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
 
 public class ClanSockets {
 
-    private static final ClanManager manager = new ClanManager();
+    private static final ClanManager manager = SimpleClans.getInstance().getClanManager();
     private static final Gson gson = new Gson();
 
     public static void onMessage(String msg, JsonObject data) {
         String id = data.get("id").getAsString();
         switch (msg) {
             case "getclans":
-                List<Clan> clans = manager.getClans();
-                send("getclans_"+id, gson.toJsonTree(clans).getAsJsonObject(), 200);
+                try {
+                    List<Clan> clans = manager.getClans();
+                    JsonObject json = new JsonObject();
+                    json.add("clans", gson.toJsonTree(clans));
+                    send("getclans_"+id, json, 200);
+                } catch (Exception e) {
+                    e.printStackTrace();                    
+                }
                 break;
             case "heartbeat":
                 send("heartbeat_"+id, new JsonObject(), 200);                
